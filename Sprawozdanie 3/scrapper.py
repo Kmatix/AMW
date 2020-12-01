2bddcd0931df69f6479f6f67d4bb6b96cfb059d5
Kmatix <kmatix888@gmail.com>
2020-12-01 18:38:06 +0100

    zad3

---
+++ Sprawozdanie 3/scrapper.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+result = requests.get("https://lowcygier.pl/")
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+
+newest_post = soup.find('h2', class_='post-title')
+for a in newest_post.find_all('a', href=True):
+    print(a['href'])
+
+input()
+
